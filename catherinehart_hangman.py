4074fae064b5590a4c1d8319ccb644b580fe9e32
catherinehart20 <65929295+catherinehart20@users.noreply.github.com>
2020-06-04 19:56:30 -0400

    Create catherinehart_hangman.py

---
+++ catherinehart_hangman.py
@@ -0,0 +1,17 @@
+def main():
+    word = 'hangman'
+    print(' '.join(word.upper()))
+    print('The game will be available soon.')
+    
+if __name__ == '__main__':
+    main()
+```print(H A N G M A N)
+
+word = input("Guess the word: ")
+
+if word == "python":
+    print("You survived!")
+elif word == "java":
+    print("You are hanged!")```
+
+*comment: There was a syntax error that i could not figure out on line 8... I will finish the code asap
