d2362e3bd5eca9d6a85949ac454181bf0d34ea08
catherinehart20 <65929295+catherinehart20@users.noreply.github.com>
2020-06-11 19:46:46 -0400

    Create catherinehart_rockpaperscissor.py

---
+++ catherinehart_rockpaperscissor.py
@@ -0,0 +1,16 @@
+import random 
+ 
+mylist = ['Rock','Paper','Scissor']
+
+x = random.choice(mylist)
+print(x)
+
+
+
+string = 'Hello I am catherine and I enjoy learning python'
+nstring = string.split() #splits a string and puts into a list
+mylist = [1,2,3,4]
+print(max(mylist))
+print(min(mylist))
+print(len(mylist))
+print(nstring) 
